b6257856708e68e8e4f409fea26c2b96d95032e5
Chris Kent <Chris_Kent@ymail.com>
2022-08-09 22:59:46 -0400

    Completed Lab

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by leon on 3/6/18.
@@ -46,11 +46,46 @@ public class ArrayUtility <T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
-    }
+        Integer max = 0;
+        Boolean change = true;
+        T returnKey = null;
+        Integer keyPlace = 1;
+        Integer count = 0;
+        ArrayList<T> combinedList = new ArrayList<>(inputArray.length + arrayToMerge.length);
+        combinedList.addAll(Arrays.asList(inputArray));
+        combinedList.addAll(Arrays.asList(arrayToMerge));
+        HashMap<T, Integer> map = new HashMap<>();
+        for (T t : combinedList) {
+            if (!map.containsKey(t)) {
+                map.put(t, 1);
+            } else {
+                count = map.get(t) + 1;
+                map.put(t, count);
+            }
+        }
+        while(change == true) {
+            for (Map.Entry<T, Integer> entry : map.entrySet()) {
+                change = false;
+                if (entry.getValue() > max) {
+                    max = entry.getValue();
+                    returnKey = entry.getKey();
+                    change = true;
+                }
+            }
+        }
+            return returnKey;
+        }
+
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return null;
+        Integer count1 = getNumberOfOccurrences(valueToEvaluate);
+        Integer count2 = 0;
+        for (int i = 0; i < arrayToMerge.length; i++) {
+            if(arrayToMerge[i] == valueToEvaluate){
+                count2++;
+            }
+        }
+        return count1 + count2;
     }
 
 
